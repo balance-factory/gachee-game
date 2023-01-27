5835f117b0f74139c6eb5a38ade6c0f09aba707f
hwang-jaewon <hhjaewon@gmail.com>
2023-01-27 15:26:41 +0900

    [Modify] 라우트 수정

--- src/App.tsx
+++ src/App.tsx
@@ -8,10 +8,11 @@ const App = () => {
             <Route path="/login" element={<Pages.LoginView />} />
             <Route path="/guide" element={<Pages.GuideView />} />
             <Route path="/category" element={<Pages.CategoryView />} />
-            <Route path="/question" element={<Pages.QuestionView />} /> // 카테고리Id, 문제Id는 쿼리로
-            <Route path="/match-list/:userId" element={<Pages.MatchListView />} />
-            <Route path="/result/:userAId/:userBId" element={<Pages.ResultView />} />
-            <Route path="/answer/:userId" element={<Pages.ResultView />} />
+            <Route path="/question/:categoryId" element={<Pages.QuestionView />} /> // 카테고리Id, 문제Id는 쿼리로
+            <Route path="/match-list" element={<Pages.MatchListView />} />
+            <Route path="/result" element={<Pages.ResultView />} />
+            <Route path="/answer" element={<Pages.ResultView />} />
+            {/* 링크 공유주소 "?requester-id=&participater-id=" */}
         </Routes>
     );
 };
