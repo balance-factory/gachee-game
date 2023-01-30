a0cfa014e5cf7ab4ba446e6b44497a2f8c82a17d
hwang-jaewon <hhjaewon@gmail.com>
2023-01-30 17:36:05 +0900

    [Add] 카카오톡 로그인 토큰 받기까지 완료

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,6 +1,35 @@
 import styled from "styled-components";
+import { useEffect } from "react";
+import { REST_API_KEY, REDIRECT_URI } from "../../KakaoOAuth";
 
 const Login = () => {
+    const KAKAO_CODE = new URL(location.href).searchParams.get("code");
+
+    const getKakaoToken = () => {
+        console.log("getKakaoToken 실행됨");
+        fetch(`https://kauth.kakao.com/oauth/token`, {
+            method: "POST",
+            headers: { "Content-Type": "application/x-www-form-urlencoded;charset=utf-8" },
+            body: `grant_type=authorization_code&client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&code=${KAKAO_CODE}`,
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data, "data ");
+
+                if (data.access_token) {
+                    localStorage.setItem("token", data.access_token);
+                    // window.location.href = "/guide";
+                } else {
+                    window.location.href = "/";
+                }
+            });
+    };
+
+    useEffect(() => {
+        if (!location.search) return;
+        getKakaoToken();
+    }, []);
+
     return <Header>로그인 페이지</Header>;
 };
 
