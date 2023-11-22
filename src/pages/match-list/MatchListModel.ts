05ffd633b0d612c6d997bf94248c4a3e66c2fbd2
dodoheeee <hiyoun96@naver.com>
2023-11-23 00:26:39 +0900

    fix : match-list view

--- src/pages/match-list/MatchListModel.ts
+++ src/pages/match-list/MatchListModel.ts
@@ -2,6 +2,10 @@ import * as Interface from "../../interface";
 import { api } from "../../api";
 
 export const getMatchedUsers = async (categoryId: number) => {
-    const { data } = await api.get<Interface.MatchedUser[]>(`/memberAnswer/matchedMemberList?categoryId=${categoryId}`);
-    return data;
+    const { data } = await api.get<{
+        body: {
+            matchedMemberList: Interface.MatchedUser[];
+        };
+    }>(`/memberAnswer/matchedMemberList?categoryId=${categoryId}`);
+    return data.body.matchedMemberList;
 };
