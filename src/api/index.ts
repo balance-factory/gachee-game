544ddf6abb24e19c880393849c1726160de10353
dodoheeee <hiyoun96@naver.com>
2023-12-19 01:16:43 +0900

    Add : 로컬스토리지 정보 업데이트 시켜주는 로직 추가

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,78 +3,74 @@ import * as Interface from "interface";
 import { useNavigate } from "react-router-dom";
 
 type Params = {
-  [key: string]: unknown;
+    [key: string]: unknown;
 };
 
-export const baseUrl = location.href.includes("localhost")
-  ? "http://kalzake.gonetis.com:8080"
-  : "https://kor1du.shop";
+export const baseUrl = location.href.includes("localhost") ? "http://kalzake.gonetis.com:8080" : "https://kor1du.shop";
 export const errorHandle = {
-  callback: (v: boolean) => {},
+    callback: (v: boolean) => {},
 };
 
 export const api = {
-  get: <T>(url: string, params?: Params) =>
-    axios.get<T>(baseUrl + url, { params: { ...params } }),
-  post: <T, R = unknown, E = unknown>(url: string, data?: R, headers?: E) =>
-    axios.post<T>(baseUrl + url, data || {}, { ...headers }),
-  put: <T, R = unknown, E = unknown>(url: string, data?: R, headers?: E) =>
-    axios.put<T>(baseUrl + url, data || undefined, { ...headers }),
-  delete: <T>(url: string) => axios.delete<T>(baseUrl + url),
+    get: <T>(url: string, params?: Params) => axios.get<T>(baseUrl + url, { params: { ...params } }),
+    post: <T, R = unknown, E = unknown>(url: string, data?: R, headers?: E) =>
+        axios.post<T>(baseUrl + url, data || {}, { ...headers }),
+    put: <T, R = unknown, E = unknown>(url: string, data?: R, headers?: E) =>
+        axios.put<T>(baseUrl + url, data || undefined, { ...headers }),
+    delete: <T>(url: string) => axios.delete<T>(baseUrl + url),
 };
 
 axios.interceptors.response.use(
-  function (response) {
-    return response;
-  },
-  async function (error) {
-    if (error.response.status === 401) {
-      const navigate = useNavigate();
-      // 401: Unauthorized
-      // localStorage에서 refreshToken을 가져와서 재발급 요청
-      // 재발급 요청이 실패하면 로그인 페이지로 이동
-      // 재발급 요청이 성공하면 accessToken을 localStorage에 저장하고
-      // 요청했던 API를 다시 요청
-      try {
-        const originalRequest = error.config;
-        localStorage.removeItem("accessToken");
-        const refreshToken = localStorage.getItem("refreshToken");
-        const result = await api.get<{
-          body: {
-            jwtToken: Interface.TokenInfo;
-            memberInfo: Interface.MemberInfo;
-          };
-        }>(`/token/reissue?refreshToken=${refreshToken}`);
+    function (response) {
+        return response;
+    },
+    async function (error) {
+        if (error.response.status === 401) {
+            const navigate = useNavigate();
+            // 401: Unauthorized
+            // localStorage에서 refreshToken을 가져와서 재발급 요청
+            // 재발급 요청이 실패하면 로그인 페이지로 이동
+            // 재발급 요청이 성공하면 accessToken을 localStorage에 저장하고
+            // 요청했던 API를 다시 요청
+            try {
+                const originalRequest = error.config;
+                localStorage.removeItem("accessToken");
+                const refreshToken = localStorage.getItem("refreshToken");
+                const result = await api.get<{
+                    body: {
+                        jwtToken: Interface.TokenInfo;
+                        memberInfo: Interface.MemberInfo;
+                    };
+                }>(`/token/reissue?refreshToken=${refreshToken}`);
 
-        if (result) {
-          localStorage.setItem(
-            "accessToken",
-            result.data.body.jwtToken.accessToken
-          );
-          localStorage.setItem(
-            "refreshToken",
-            result.data.body.jwtToken.refreshToken
-          );
-          return await axios.request(originalRequest);
-        } else {
-          navigate("/");
+                if (result) {
+                    localStorage.setItem("accessToken", result.data.body.jwtToken.accessToken);
+                    localStorage.setItem("refreshToken", result.data.body.jwtToken.refreshToken);
+                    localStorage.setItem(
+                        "accessTokenExpiresIn",
+                        result.data.body.jwtToken.accessTokenExpiresIn.toString()
+                    );
+                    window.sessionStorage.setItem("my-user-id", result.data.body.memberInfo.memberId);
+                    return await axios.request(originalRequest);
+                } else {
+                    navigate("/");
+                }
+            } catch (e) {
+                navigate("/");
+                return Promise.reject(error);
+            }
         }
-      } catch (e) {
-        navigate("/");
-        return Promise.reject(error);
-      }
-    }
 
-    return Promise.reject(error.response.data);
-  }
+        return Promise.reject(error.response.data);
+    }
 );
 
 axios.interceptors.request.use(function (config: any) {
-  if (config.url.indexOf("/") > -1 || config.url.indexOf("/") > -1) {
-    config.headers = {
-      ...config.headers,
-      Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
-    };
-  }
-  return config;
+    if (config.url.indexOf("/") > -1 || config.url.indexOf("/") > -1) {
+        config.headers = {
+            ...config.headers,
+            Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+        };
+    }
+    return config;
 });
