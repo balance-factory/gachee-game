c167433310194d37a16c7ed82709b96946cc8de3
jaewon <hhjaewon@gmail.com>
2023-12-22 18:09:19 +0900

    fix: url 수정

--- src/KakaoOAuth.tsx
+++ src/KakaoOAuth.tsx
@@ -1,4 +1,4 @@
 export const REST_API_KEY = "ba328d6c6c8b3f28bb0d9d057192ff6b";
 export const REDIRECT_URI = location.href.includes("localhost")
   ? "http://localhost:3000/login"
-  : "https://gachee-game.vercel.app/login";
+  : "https://gachee-game-main.vercel.app/login";
